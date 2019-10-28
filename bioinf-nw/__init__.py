4b7d220b9a83dc6ecda038cfd214ed4bb42d4d1c
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-10-28 17:17:01 +0100

    Init

---
+++ bioinf-nw/__init__.py
@@ -0,0 +1,5 @@
+"""bioinf-nw - An opinionated, minimal cookiecutter template for Python packages"""
+
+__version__ = '0.1.0'
+__author__ = 'Paweł Rzepiński <rzepinski.pawel@gmail.com>'
+__all__ = []
