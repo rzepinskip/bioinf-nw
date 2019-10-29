25f6b9e262c2e05939248171a84651547675faf6
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-10-29 11:31:34 +0100

    Add algorithm implementation

--- bionw/algo.py
+++ bionw/algo.py
@@ -8,13 +8,69 @@ class NWAlgo:
         self._config = config
 
     def compute_cost_matrix(self, first_seq: str, second_seq: str) -> Any:
-        pass
+        cost_matrix = numpy.zeros(((len(first_seq) + 1, len(second_seq) + 1)))
+        for i in range(1, (len(first_seq) + 1)):
+            cost_matrix[i, 0] = i * self._config.gap
+        for i in range(1, (len(second_seq)) + 1):
+            cost_matrix[0, i] = i * self._config.gap
+
+        for i in range(1, (len(first_seq) + 1)):
+            for j in range(1, (len(second_seq) + 1)):
+                left = cost_matrix[i, j - 1] + self._config.gap
+                up = cost_matrix[i - 1, j] + self._config.gap
+                comp = self._config.same if first_seq[i - 1] == second_seq[j - 1] else self._config.diff
+                diag = cost_matrix[i - 1, j - 1] + comp
+                cost_matrix[i, j] = max(left, up, diag)
+
+        return cost_matrix
 
     def get_all_alignments(self, first_seq: str, second_seq: str, cost_matrix: Any):
-        pass
+        graph = self._retrieve_previous_nodes(first_seq, second_seq, cost_matrix)
+        paths = self._find_all_paths(graph, (len(first_seq), len(second_seq)), (0, 0), list())
+        alignments: List[Tuple[str, str]] = list()
+        for path in paths:
+            first_aligned = ""
+            second_aligned = ""
+            previous_node = (len(first_seq), len(second_seq))
+
+            for node in path[1:]:
+                i, j = previous_node
+                if i == node[0]:
+                    first_aligned = "-" + first_aligned
+                    second_aligned = second_seq[j - 1] + second_aligned
+                elif j == node[1]:
+                    first_aligned = first_seq[i - 1] + first_aligned
+                    second_aligned = "-" + second_aligned
+                else:
+                    first_aligned = first_seq[i - 1] + first_aligned
+                    second_aligned = second_seq[j - 1] + second_aligned
+
+                previous_node = node
+
+            alignments += [(first_aligned, second_aligned)]
+
+        return alignments
 
     def _retrieve_previous_nodes(self, first_seq: str, second_seq: str, cost_matrix: Any):
-        pass
+        graph = dict()
+        for i in range(1, (len(first_seq) + 1))[::-1]:
+            graph[(i, 0)] = [(i - 1, 0)]
+            graph[(0, i)] = [(0, i - 1)]
+
+            for j in range(1, (len(second_seq) + 1))[::-1]:
+                graph[(i, j)] = list()
+                left = cost_matrix[i, j - 1] + self._config.gap
+                up = cost_matrix[i - 1, j] + self._config.gap
+                comp = self._config.same if first_seq[i - 1] == second_seq[j - 1] else self._config.diff
+                diag = cost_matrix[i - 1, j - 1] + comp
+                if cost_matrix[i, j] == left:
+                    graph[(i, j)] += [(i, j - 1)]
+                if cost_matrix[i, j] == up:
+                    graph[(i, j)] += [(i - 1, j)]
+                if cost_matrix[i, j] == diag:
+                    graph[(i, j)] += [(i - 1, j - 1)]
+
+        return graph
 
     def _find_all_paths(
         self, 
@@ -23,4 +79,22 @@ class NWAlgo:
         end: Tuple[int, int],
         path: List[Tuple[int, int]]
     ) -> List[List[Tuple[int, int]]]:
-        pass
+        path = path + [start]
+        if start == end:
+            return [path]
+        if start not in graph:
+            return list()
+
+        paths: List[List[Tuple[int, int]]] = list()
+        for node in graph[start]:
+            if node not in path:
+                new_paths = self._find_all_paths(graph, node, end, path)
+                paths += new_paths
+
+                if len(paths) > self._config.max_paths:
+                    warnings.warn(
+                        f"Too many paths found. Truncating to {self._config.max_paths}", stacklevel=2
+                    )
+                    return paths
+
+        return paths
